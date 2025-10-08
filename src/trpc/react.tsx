acac8c940358970213ee5c3b6874d5f401ed0dfa
Zimraan <zimraan2012@gmail.com>
2025-10-08 15:39:42 +1100

    changes to base path = /app

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -72,7 +72,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
 }
 
 function getBaseUrl() {
-  if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return `http://localhost:${process.env.PORT ?? 3000}`;
+  if (typeof window !== "undefined") return window.location.origin + "/app";
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}/app`;
+  return `http://localhost:${process.env.PORT ?? 3000}/app`;
 }
